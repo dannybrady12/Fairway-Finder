696b6196d015bf5251bb911434c1627eee2bd59c
dannybrady12 <dbrady716@gmail.com>
2025-06-02 18:25:13 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
 'use client';
 
+import { useState } from 'react';
 import { createBrowserClient } from '@supabase/ssr';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { useState } from 'react';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [supabaseClient] = useState(() => {
-    return createBrowserClient(
+  const [supabaseClient] = useState(() =>
+    createBrowserClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    );
-  });
+    )
+  );
 
   return (
     <html lang="en">
